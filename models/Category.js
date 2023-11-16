62027081094c9c1ba0fc0b30d02f00765bc8cdda
Ferny <fernc28@outlook.com>
2023-11-15 20:30:42 -0500

    commenting lines of code

--- models/Category.js
+++ models/Category.js
@@ -1,29 +1,37 @@
+// Import necessary modules from Sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+// Import the Sequelize instance from the connection module
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+// Create a Category class that extends Sequelize's Model class
+class Category extends Model { }
 
+// Initialize the Category model with column definitions and other options
 Category.init(
   {
-    id: { 
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
+    // Define the 'id' column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
+    // Define the 'category_name' column
     category_name: {
       type: DataTypes.STRING,
       allowNull: false
     }
   },
   {
+    // Pass the Sequelize instance for this model
     sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
+    timestamps: false, // Disable timestamps for this model
+    freezeTableName: true, // Use the model name as the table name
+    underscored: true, // Use snake_case for column names
+    modelName: 'category', // Set the model name to 'category'
   }
 );
 
+// Export the Category model for use in other modules
 module.exports = Category;
